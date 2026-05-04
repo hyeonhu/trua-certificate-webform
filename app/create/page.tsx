7024487d5fd6a78b7e563c7cd6e0d455b467b49e
hyeonhu <tjdals2428@gmail.com>
2026-05-04 15:31:27 +0900

    Delete app directory

--- app/create/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CustomerEditor } from "@/components/CustomerEditor";
-import { getSiteConfig } from "@/lib/site-config";
-import { getTemplateConfig } from "@/lib/template-config";
-
-export default async function CreatePage() {
-  const config = await getTemplateConfig();
-  const siteConfig = await getSiteConfig();
-  return <CustomerEditor initialConfig={config} siteConfig={siteConfig} />;
-}
